5237acc3f66889cabdf4720d756e199ed6d9fd0a
nagayama <nagayama@iroirodesign.com>
2024-06-04 16:37:31 +0900

    update Code Component

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,8 +4,6 @@ const docsCollection = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
-    subTitle: z.string().optional(),
-    description: z.string().optional(),
     lastUpdated: z.string().optional(),
   }),
 });
