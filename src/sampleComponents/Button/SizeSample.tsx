c964390f53cdaddb2ae6c844e21dc4cc779414d3
degoo <degoo@takram.com>
2024-06-19 23:21:25 +0900

    Tweak sample codes

---
+++ src/sampleComponents/Button/SizeSample.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@serendie/ui";
+import { HBox } from "src/components/LayoutUtils";
+
+export function SizeSample() {
+  return (
+    <HBox>
+      <Button size="small">Small</Button>
+      <Button size="medium">Medium</Button>
+    </HBox>
+  );
+}
--- src/sampleComponents/Button/StateSample.tsx
+++ src/sampleComponents/Button/StateSample.tsx
@@ -1,22 +1,6 @@
 import { Button } from "@serendie/ui";
-import { HBox, VBox } from "src/components/LayoutUtils";
 import type { ComponentProps } from "react";
 import { StateMatrix } from "src/components/StateMatrix";
-
-export function SizeSample() {
-  return (
-    <HBox>
-      <VBox>
-        <Button size="small">Click me</Button>
-        <p>Small</p>
-      </VBox>
-      <VBox>
-        <Button size="medium">Click me</Button>
-        <p>Medium</p>
-      </VBox>
-    </HBox>
-  );
-}
 
 export const StateSample: React.FC = () => {
   return (
