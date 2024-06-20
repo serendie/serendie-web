b71889525b6dae5d3674613d7293cc0af46067ae
yahiro <133099806+centeringbook@users.noreply.github.com>
2024-06-20 18:23:36 +0900

    Add Progress Indicator

---
+++ src/sampleCode/ProgressIndicator/SizeSample.tsx
@@ -0,0 +1,21 @@
+import { IconButton, ProgressIndicator } from "@serendie/ui";
+import { HBox, VBox } from "src/components/LayoutUtils";
+
+export function SizeSample() {
+  return (
+    <HBox>
+      <VBox>
+        <ProgressIndicator size="small" />
+        <p>Small</p>
+      </VBox>
+      <VBox>
+        <ProgressIndicator size="medium" />
+        <p>Medium</p>
+      </VBox>
+      <VBox>
+        <ProgressIndicator size="large" />
+        <p>Large</p>
+      </VBox>
+    </HBox>
+  );
+}
