cf732e5e5d0801f2bacb8075b120f9ca82150bac
yahiro <133099806+centeringbook@users.noreply.github.com>
2024-06-20 18:34:18 +0900

    Add Search

---
+++ src/sampleCode/Search/StateSample.tsx
@@ -0,0 +1,29 @@
+import { Search, Select } from "@serendie/ui";
+import { Dd, Dl, Dt } from "src/components/LayoutUtils";
+
+export function StateSample() {
+  return (
+    <Dl>
+      <Dt>Enabled</Dt>
+      <Dd>
+        <Search items={["React", "Vue", "Angular", "Svelte", "Ember"]} />
+      </Dd>
+
+      <Dt>Filled</Dt>
+      <Dd>
+        <Search
+          items={["React", "Vue", "Angular", "Svelte", "Ember"]}
+          value={["React"]}
+        />
+      </Dd>
+
+      <Dt>Disabled</Dt>
+      <Dd>
+        <Search
+          items={["React", "Vue", "Angular", "Svelte", "Ember"]}
+          disabled
+        />
+      </Dd>
+    </Dl>
+  );
+}
