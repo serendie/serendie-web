8680c2a609dfdd8c0872af433a6b6dffaa1a4fb7
yahiro <133099806+centeringbook@users.noreply.github.com>
2024-06-21 13:56:35 +0900

    Wip

---
+++ src/sampleCode/TopAppBar/TypeSample.tsx
@@ -0,0 +1,12 @@
+import { Dd, Dl, Dt } from "@/components/LayoutUtils";
+
+export function TypeSample() {
+  return (
+    <Dl>
+      <Dt>Default</Dt>
+      <Dd>
+        <TopAppBar />
+      </Dd>
+    </Dl>
+  );
+}
