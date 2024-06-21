db986e70772b8a97415b5ae0f4d9b8bafb94b2db
yahiro <133099806+centeringbook@users.noreply.github.com>
2024-06-21 14:14:09 +0900

    Add Top App Bar

--- src/sampleCode/TopAppBar/TypeSample.tsx
+++ src/sampleCode/TopAppBar/TypeSample.tsx
@@ -1,11 +1,79 @@
 import { Dd, Dl, Dt } from "@/components/LayoutUtils";
+import { IconButton } from "@serendie/ui";
+import { TopAppBar } from "@serendie/ui";
 
 export function TypeSample() {
   return (
     <Dl>
-      <Dt>Default</Dt>
+      <Dt>Navbar</Dt>
       <Dd>
-        <TopAppBar />
+        <TopAppBar
+          type="navbar"
+          headingIconButton={
+            <IconButton icon="menu" styleType="ghost" shape="rectangle" />
+          }
+          trailingIconButtons={
+            <IconButton icon="search" styleType="ghost" shape="rectangle" />
+          }
+        />
+      </Dd>
+
+      <Dt>Title Bar</Dt>
+      <Dd>
+        <TopAppBar
+          type="titleBar"
+          title="Default"
+          trailingIconButtons={
+            <IconButton icon="add" styleType="ghost" shape="rectangle" />
+          }
+        />
+      </Dd>
+
+      <Dt>Navbar + Title Bar</Dt>
+      <Dd>
+        <TopAppBar
+          type="navbar"
+          headingIconButton={
+            <IconButton icon="menu" styleType="ghost" shape="rectangle" />
+          }
+          trailingIconButtons={
+            <IconButton icon="search" styleType="ghost" shape="rectangle" />
+          }
+        />
+        <TopAppBar
+          type="titleBar"
+          title="Default"
+          trailingIconButtons={
+            <IconButton icon="add" styleType="ghost" shape="rectangle" />
+          }
+        />
+      </Dd>
+
+      <Dt>Navbar + Title Bar</Dt>
+      <Dd>
+        <TopAppBar
+          type="navbar"
+          headingIconButton={
+            <IconButton icon="menu" styleType="ghost" shape="rectangle" />
+          }
+          trailingIconButtons={
+            <IconButton icon="search" styleType="ghost" shape="rectangle" />
+          }
+        />
+        <TopAppBar
+          type="titleBar"
+          title="Default"
+          headingIconButton={
+            <IconButton
+              icon="chevron_left"
+              styleType="ghost"
+              shape="rectangle"
+            />
+          }
+          trailingIconButtons={
+            <IconButton icon="add" styleType="ghost" shape="rectangle" />
+          }
+        />
       </Dd>
     </Dl>
   );
