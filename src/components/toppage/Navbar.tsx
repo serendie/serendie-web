6b60810ae2db470feaa9cc1f7756a7837725a78b
yahiro <133099806+centeringbook@users.noreply.github.com>
2024-08-15 11:12:52 +0900

    Add motion

---
+++ src/components/toppage/Navbar.tsx
@@ -0,0 +1,84 @@
+import { styled } from "@serendie/ui/jsx";
+
+export const Navbar: React.FC = () => {
+  const Navbar = styled("nav", {
+    base: {
+      paddingTop: "24px",
+      paddingBottom: "24px",
+      paddingLeft: "40px",
+      paddingRight: "40px",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      color: "sd.system.color.impression.onPrimary",
+    },
+  });
+
+  const NavbarLogo = styled("div", {
+    base: {
+      display: "flex",
+      listStyle: "none",
+      padding: 0,
+      gap: "24px",
+      margin: 0,
+    },
+  });
+
+  const NavbarTitle = styled("h1", {
+    base: {
+      textStyle: "sd.system.typography.title.large_expanded",
+    },
+  });
+
+  const NavbarLinkContainer = styled("ul", {
+    base: {
+      height: "100%",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      listStyle: "none",
+      padding: 0,
+      gap: "24px",
+      margin: 0,
+    },
+  });
+
+  const NavbarLink = styled("li", {
+    base: {
+      textStyle: "sd.system.typography.title.small_expanded",
+    },
+  });
+
+  return (
+    <Navbar>
+      <NavbarLogo>
+        <svg
+          width="22"
+          height="24"
+          viewBox="0 0 22 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M20.4516 18.5136C19.4705 20.1995 18.1397 21.5368 16.4648 22.5227C14.787 23.5085 12.9529 24 10.9623 24C8.97174 24 7.13758 23.5057 5.45983 22.5227C3.78207 21.5368 2.45123 20.1995 1.47017 18.5136C0.489109 16.8277 0 14.9846 0 12.9844H2.6446C2.6446 14.4989 3.01997 15.899 3.77638 17.1849C4.52995 18.4708 5.54514 19.4852 6.82478 20.2281C8.10443 20.9711 9.4836 21.3425 10.9623 21.3425C12.441 21.3425 13.8628 20.9625 15.1425 20.2053C16.4221 19.448 17.4316 18.4336 18.171 17.162C18.9103 15.8905 19.28 14.4989 19.28 12.9844H21.9246C21.9246 14.9846 21.4355 16.8277 20.4544 18.5136H20.4516ZM7.82859 11.4642C7.41626 10.4784 7.20868 9.41541 7.20868 8.27241C7.20868 6.75795 7.57835 5.37207 8.3177 4.11477C9.05705 2.85748 10.0523 1.85736 11.3035 1.11442C12.5548 0.371473 13.9339 0 15.4411 0C16.5785 0 17.6392 0.208596 18.6174 0.620074C19.5985 1.03441 20.4743 1.62877 21.2421 2.40029L19.3653 4.28622C18.825 3.7433 18.2364 3.33754 17.5966 3.06608C16.9567 2.79462 16.2544 2.65746 15.4866 2.65746C14.4628 2.65746 13.5159 2.90892 12.6486 3.40612C11.7813 3.90618 11.0988 4.59197 10.6012 5.46351C10.1035 6.33504 9.85612 7.27229 9.85612 8.27241C9.85612 9.78688 10.3964 11.1013 11.477 12.2157L9.60019 14.1017C8.8324 13.3302 8.24092 12.4529 7.83144 11.4671L7.82859 11.4642Z"
+            fill="white"
+          />
+        </svg>
+
+        <NavbarTitle>Serendie Design System</NavbarTitle>
+      </NavbarLogo>
+
+      <NavbarLinkContainer>
+        <NavbarLink>
+          <a href="/">基本の情報</a>
+        </NavbarLink>
+        <NavbarLink>
+          <a href="/">スタイル</a>
+        </NavbarLink>
+        <NavbarLink>
+          <a href="/">デザインシステム</a>
+        </NavbarLink>
+      </NavbarLinkContainer>
+    </Navbar>
+  );
+};
---
+++ src/components/toppage/Shapes.tsx
@@ -0,0 +1,233 @@
+import { motion } from "framer-motion";
+import { cubicBezier } from "framer-motion/dom";
+
+const transition = () => ({
+  duration: 1,
+  delay: Math.floor(Math.random() * 6) / 10 + -0.3,
+  ease: cubicBezier(0.84, 0, 0.16, 1),
+});
+
+export const Shapes = () => {
+  return (
+    <>
+      {[Shapes1, Shapes2, Shapes3, Shapes4, Shapes5, Shapes6].map(
+        (Shape, index) => (
+          <motion.div key={index}>
+            <Shape />
+          </motion.div>
+        )
+      )}
+    </>
+  );
+};
+
+export const Shapes1: React.FC = () => {
+  return (
+    <motion.svg
+      width="123"
+      height="135"
+      viewBox="0 0 123 135"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{
+        position: "absolute",
+        width: "123px",
+        height: "135px",
+        scale: "1.2",
+        zIndex: "-1",
+        transformOrigin: "center center",
+      }}
+      initial={{
+        top: "0",
+        left: "50%",
+        rotate: "100deg",
+      }}
+      animate={{
+        top: "calc(50vh - 67.5px)",
+        left: "70%",
+        rotate: "0deg",
+      }}
+      transition={transition()}
+    >
+      <path
+        d="M113.923 103.434C108.463 112.847 101.058 120.314 91.7378 125.818C82.4019 131.322 72.1957 134.066 61.1191 134.066C50.0426 134.066 39.8364 131.306 30.5005 125.818C21.1645 120.314 13.7591 112.847 8.29995 103.434C2.8408 94.0212 0.119141 83.7307 0.119141 72.5627H14.8351C14.8351 81.0184 16.9238 88.836 21.1329 96.0155C25.3261 103.195 30.9752 108.859 38.0958 113.007C45.2164 117.155 52.8909 119.229 61.1191 119.229C69.3474 119.229 77.2592 117.107 84.3799 112.879C91.5005 108.651 97.1179 102.987 101.232 95.8878C105.346 88.7882 107.403 81.0184 107.403 72.5627H122.119C122.119 83.7307 119.397 94.0212 113.938 103.434H113.923ZM43.6815 64.075C41.3871 58.5708 40.232 52.6358 40.232 46.2541C40.232 37.7983 42.2891 30.0605 46.4032 23.0406C50.5173 16.0207 56.0556 10.4367 63.018 6.28858C69.9804 2.14046 77.6548 0.0664062 86.0413 0.0664062C92.3708 0.0664062 98.273 1.23107 103.716 3.52849C109.175 5.84186 114.049 9.16035 118.321 13.468L107.878 23.9978C104.871 20.9665 101.596 18.701 98.0356 17.1854C94.4753 15.6697 90.5669 14.9039 86.2945 14.9039C80.598 14.9039 75.3287 16.3079 70.5025 19.0839C65.6763 21.8759 61.8787 25.7049 59.1095 30.571C56.3404 35.437 54.9638 40.6701 54.9638 46.2541C54.9638 54.7098 57.9703 62.0488 63.9832 68.271L53.5396 78.8008C49.2673 74.4931 45.976 69.5952 43.6974 64.0909L43.6815 64.075Z"
+        fill="#EB3D1C"
+      />
+    </motion.svg>
+  );
+};
+
+export const Shapes2: React.FC = () => {
+  return (
+    <motion.svg
+      width="139"
+      height="81"
+      viewBox="0 0 139 81"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{
+        position: "absolute",
+        width: "123px",
+        height: "135px",
+        scale: "1.2",
+        zIndex: "-1",
+        transformOrigin: "center center",
+      }}
+      initial={{
+        top: "40vh",
+        left: "90%",
+        rotate: "100deg",
+      }}
+      animate={{
+        top: "calc(35vh - 67.5px)",
+        left: "65%",
+        rotate: "0deg",
+      }}
+      transition={transition()}
+    >
+      <path
+        d="M25.0733 18.1831C26.0046 23.9613 28.0648 29.4995 31.1365 34.4815C34.2081 39.4635 38.2309 43.7916 42.9752 47.2189C47.7196 50.6462 53.0925 53.1054 58.7872 54.4563C64.482 55.8071 70.387 56.023 76.1652 55.0917C81.9434 54.1604 87.4816 52.1001 92.4636 49.0285C97.4456 45.9569 101.774 41.9341 105.201 37.1897C108.628 32.4454 111.088 27.0725 112.438 21.3777C113.789 15.683 114.005 9.77791 113.074 3.99971"
+        stroke="#0E5651"
+        stroke-width="50"
+      />
+    </motion.svg>
+  );
+};
+
+export const Shapes3: React.FC = () => {
+  return (
+    <motion.svg
+      width="123"
+      height="144"
+      viewBox="0 0 123 144"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{
+        position: "absolute",
+        width: "123px",
+        height: "144px",
+        zIndex: "-1",
+        transformOrigin: "center center",
+      }}
+      initial={{
+        top: "10%",
+        left: "95%",
+        rotate: "100deg",
+      }}
+      animate={{
+        top: "55vh",
+        left: "80%",
+        rotate: "-34.6deg",
+      }}
+      transition={transition()}
+    >
+      <rect opacity="0.5" y="44.2969" width="78" height="120" fill="white" />
+    </motion.svg>
+  );
+};
+
+export const Shapes4: React.FC = () => {
+  return (
+    <motion.svg
+      width="319"
+      height="319"
+      viewBox="0 0 319 319"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{
+        position: "absolute",
+        zIndex: "-1",
+        transformOrigin: "center center",
+      }}
+      initial={{
+        top: "70vh",
+        left: "50%",
+        rotate: "100deg",
+      }}
+      animate={{
+        top: "50vh",
+        left: "70%",
+        rotate: "0deg",
+      }}
+      transition={transition()}
+    >
+      <path
+        d="M17.9995 246.651C32.9721 261.623 50.7471 273.5 70.3097 281.603C89.8722 289.706 110.839 293.877 132.014 293.877C153.188 293.877 174.155 289.706 193.718 281.603C213.28 273.5 231.055 261.623 246.028 246.651C261 231.678 272.877 213.903 280.98 194.341C289.083 174.778 293.254 153.811 293.254 132.637C293.254 111.462 289.083 90.4953 280.98 70.9327C272.877 51.3701 261 33.5951 246.028 18.6226"
+        stroke="#ECB100"
+        stroke-width="50"
+      />
+    </motion.svg>
+  );
+};
+
+export const Shapes5: React.FC = () => {
+  return (
+    <motion.svg
+      width="213"
+      height="213"
+      viewBox="0 0 213 213"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{
+        position: "absolute",
+        zIndex: "-1",
+        transformOrigin: "center center",
+      }}
+      initial={{
+        top: "30vh",
+        left: "90%",
+        rotate: "23.2deg",
+      }}
+      animate={{
+        top: "55vh",
+        left: "80%",
+        rotate: "0deg",
+      }}
+      transition={transition()}
+    >
+      <path
+        d="M211.007 37.9885C188.109 15.0909 157.054 2.22712 124.671 2.22712C92.2892 2.22712 61.2333 15.0909 38.3357 37.9885C15.438 60.8862 2.57428 91.942 2.57427 124.324C2.57427 156.706 15.438 187.762 38.3357 210.66"
+        stroke="white"
+        stroke-width="4"
+      />
+    </motion.svg>
+  );
+};
+
+export const Shapes6: React.FC = () => {
+  return (
+    <motion.svg
+      width="70"
+      height="71"
+      viewBox="0 0 70 71"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{
+        position: "absolute",
+        zIndex: "-1",
+        transformOrigin: "center center",
+      }}
+      initial={{
+        top: "70vh",
+        left: "35%",
+        rotate: "23.2deg",
+      }}
+      animate={{
+        top: "75vh",
+        left: "60%",
+        rotate: "0deg",
+      }}
+      transition={transition()}
+    >
+      <rect
+        opacity="0.4"
+        x="29.209"
+        y="0.652344"
+        width="50"
+        height="50"
+        transform="rotate(35.7448 29.209 0.652344)"
+        fill="#D1EDDE"
+      />
+    </motion.svg>
+  );
+};
