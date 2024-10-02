67f2b76cfbb618c597a53ad2534f193ac46697f4
degoo <degoo@takram.com>
2024-10-03 04:48:43 +0900

    Update Accordion

--- src/sampleCode/Accordion/TypeSample.tsx
+++ src/sampleCode/Accordion/TypeSample.tsx
@@ -4,20 +4,20 @@ import { Dd, Dl, Dt } from "src/components/LayoutUtils";
 export function TypeSample() {
   return (
     <Dl variant="dim">
-      <Dt>Trailing Icon</Dt>
+      <Dt>Right Icon</Dt>
       <Dd>
         <AccordionGroup>
           <Accordion
-            title="タイトル"
+            title="Title"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
           />
         </AccordionGroup>
       </Dd>
-      <Dt>Heading Icon</Dt>
+      <Dt>Left Icon</Dt>
       <Dd>
         <AccordionGroup>
           <Accordion
-            title="タイトル"
+            title="Title"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             isLeftIcon
           />
