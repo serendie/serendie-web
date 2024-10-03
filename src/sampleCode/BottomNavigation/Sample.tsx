29aa2e8d0ac98b3abde0a1bd5a3d8d4d47a84e18
degoo <degoo@takram.com>
2024-10-04 05:12:39 +0900

    Update bottom navigation

--- src/sampleCode/BottomNavigation/Sample.tsx
+++ src/sampleCode/BottomNavigation/Sample.tsx
@@ -3,11 +3,11 @@ import { BottomNavigation, BottomNavigationItem } from "@serendie/ui";
 export function Sample() {
   return (
     <BottomNavigation>
-      <BottomNavigationItem icon="search" label="検索" />
-      <BottomNavigationItem label="ホーム" icon="search" isActive />
-      <BottomNavigationItem icon="search" label="トーク" dot />
-      <BottomNavigationItem icon="search" label="カレンダー" count={3} />
-      <BottomNavigationItem icon="search" label="アカウント" count={100} />
+      <BottomNavigationItem icon="texture" label="検索" />
+      <BottomNavigationItem icon="texture" label="ホーム" isActive />
+      <BottomNavigationItem icon="texture" label="トーク" dot />
+      <BottomNavigationItem icon="texture" label="カレンダー" count={3} />
+      <BottomNavigationItem icon="texture" label="アカウント" count={100} />
     </BottomNavigation>
   );
 }
