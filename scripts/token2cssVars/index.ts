78947c0cd30359d9e5af34788d13aa9abb610785
nagayama <nagayama@iroirodesign.com>
2024-10-14 16:29:22 +0900

    update svg/css converters

--- scripts/token2cssVars/index.ts
+++ scripts/token2cssVars/index.ts
@@ -1,31 +1,38 @@
 import vars from "./export.json";
 
-const modes = vars[0]["_illust-color-system"]["modes"];
-
-console.log(`@layer tokens {`);
-Object.entries(modes).forEach(([modeName, mode]) => {
-  if (modeName === "KONJO") {
-    console.log(`:where(:root, :host) {`);
-  } else {
-    console.log(` [data-panda-theme='${modeName.toLowerCase()}'] {`);
-  }
-  createCssVars(mode);
-  console.log(`}`);
+vars.forEach((v) => {
+  Object.entries(v).forEach(([key, value]) => {
+    const modes = value["modes"];
+    convert(key, modes);
+  });
 });
-console.log(`}`);
+
+function convert(key, modes) {
+  console.log(`@layer tokens {`);
+  Object.entries(modes).forEach(([modeName, mode]) => {
+    if (modeName === "KONJO") {
+      console.log(`:where(:root, :host) {`);
+    } else {
+      console.log(` [data-panda-theme='${modeName.toLowerCase()}'] {`);
+    }
+    createCssVars(key, mode);
+    console.log(`}`);
+  });
+  console.log(`}`);
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-function createCssVars(obj: any, prefix: string = "") {
+function createCssVars(rootKey: string, obj: any, prefix: string = "") {
   Object.entries(obj).forEach(([key, value]) => {
     if (typeof value === "object") {
       if (value["$value"]) {
         console.log(
-          `--illust-${prefix.toLowerCase()}${key.toLowerCase()}: ${tokenValue2CssVar(
+          `--${rootKey}-${prefix.toLowerCase()}${key.toLowerCase()}: ${tokenValue2CssVar(
             value["$value"]
           )};`
         );
       } else {
-        createCssVars(value, `${prefix}${key}-`);
+        createCssVars(rootKey, value, `${prefix}${key}-`);
       }
     }
   });
