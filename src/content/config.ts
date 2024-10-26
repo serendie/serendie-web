18a03759b3a6f141a30a75bdf784da30a4cdbab7
nagayama <nagayama@iroirodesign.com>
2024-10-26 18:26:50 +0900

    add commments

--- src/content/config.ts
+++ src/content/config.ts
@@ -14,13 +14,20 @@ const pagesCollection = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
-    parent: z.string().optional(),
-    lastUpdated: z.string().optional(),
     description: z.string().optional(),
+    // ページの更新日
+    lastUpdated: z.string().optional(),
+    // ヘッダの画像のタイプ
     illustType: z.enum(["A", "B", "C", "D"]).optional(),
+    // ヘッダの画像のサイズ（これを指定するとヘッダー下部のボーダーが表示されなくなる
     illustSize: z.enum(["large", "small"]).optional(),
+    // 右サイドバーを使わずに幅ギリギリまでつかう
     fullWidth: z.boolean().optional(),
+    // ページ下部のread moreのリンク集を表示
     showSiblingLinks: z.boolean().optional(),
+    // 親ページ(↓のsubLinksと併用する)
+    parent: z.string().optional(),
+    // ヘッダー下部のタブのリンク
     subLinks: z
       .array(
         z.object({
