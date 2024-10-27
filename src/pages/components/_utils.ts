833146bd2ee0573318865373efd4ada65fe67b3e
nagayama <nagayama@iroirodesign.com>
2024-10-27 19:03:36 +0900

    _utilsとして分離する必要がないので削除

--- src/pages/components/_utils.ts
+++
@@ -1,9 +0,0 @@
-import { getCollection } from "astro:content";
-
-export const getComponentsCollection = async () => {
-  const components = await getCollection("components");
-  return components.map((entry) => ({
-    params: { slug: entry.slug },
-    props: { entry },
-  }));
-};
