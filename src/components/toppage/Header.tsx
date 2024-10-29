7fe86c87ae4c454bd4e4ae322b6d1c721d755151
degoo <degoo@takram.com>
2024-10-30 01:59:15 +0900

    Tweak maxWidth for middle layout

--- src/components/toppage/Header.tsx
+++ src/components/toppage/Header.tsx
@@ -50,10 +50,10 @@ const HeaderTitle = styled("h1", {
     textStyle: "sd.system.typography.title.large_expanded",
     fontSize: "32px",
     fontWeight: "400",
+    maxW: "410px",
     expanded: {
       fontSize: "48px",
       gap: "sd.system.dimension.spacing.twoExtraSmall",
-      maxW: "410px",
     },
   },
 });
@@ -64,9 +64,9 @@ const HeaderDescription = styled("p", {
     mt: "sd.system.dimension.spacing.medium",
     mb: "sd.system.dimension.spacing.extraLarge",
     fontWeight: "400",
+    maxW: "410px",
     expanded: {
       textStyle: "sd.system.typography.title.large_expanded",
-      maxW: "410px",
     },
   },
 });
