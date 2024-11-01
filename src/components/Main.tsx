c9961e8b5e2f590b9ed2a32643620635e9377db4
degoo <degoo@takram.com>
2024-11-01 23:18:38 +0900

    Use subtle on read-more navigation

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -28,5 +28,53 @@ export const Main = styled("main", {
       maxW: "calc(1200px + 80px * 2)",
       py: "56px",
     },
+    "& p": {
+      my: "sd.system.dimension.spacing.extraLarge",
+      textStyle: "sd.system.typography.body.small_compact",
+      sm: {
+        textStyle: "sd.system.typography.body.medium_compact",
+      },
+    },
+    "& h2": {
+      fontWeight: "sd.reference.typography.fontWeight.bold",
+      my: "sd.system.dimension.spacing.extraLarge",
+      fontSize: "20px",
+      sm: {
+        my: "sd.system.dimension.spacing.extraLarge",
+        fontSize: "24px",
+      },
+    },
+    "& h3": {
+      my: "sd.system.dimension.spacing.medium",
+      fontWeight: "sd.reference.typography.fontWeight.bold",
+      fontSize: "sd.reference.typography.scale.expanded.medium",
+      sm: {
+        my: "sd.system.dimension.spacing.extraLarge",
+        mt: "sd.system.dimension.spacing.twoExtraLarge",
+        fontSize: "sd.reference.typography.scale.compact.extraLarge",
+      },
+    },
+    "& ol": {
+      pl: "sd.system.dimension.spacing.large",
+      textStyle: "sd.system.typography.body.small_compact",
+      sm: {
+        textStyle: "sd.system.typography.body.medium_compact",
+      },
+      "& li": {
+        listStyle: "decimal",
+      },
+    },
+    "& svg": {
+      maxWidth: "100%",
+      height: "auto",
+    },
+    "& strong": {
+      fontWeight: "sd.reference.typography.fontWeight.regular",
+      color: "web.system.color.component.onSurface",
+      px: "2px",
+      pb: "1px",
+      pt: "2.5px",
+      backgroundColor: "web.system.color.impression.subtle",
+    },
   },
 });
