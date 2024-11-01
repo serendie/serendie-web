eea6df58a5565e82a05d7218b24b824830fa9edf
degoo <degoo@takram.com>
2024-11-01 23:45:49 +0900

    Tweak side description

--- siteConfig.js
+++ siteConfig.js
@@ -1,4 +1,5 @@
 export const siteConfig = {
   siteName: "Serendie Design System",
-  siteDescription: "a design system for rapid product development",
+  siteDescription:
+    "Serendie Design Systemは、多様な事業と人々をつなぎ、新たな価値を生み出すための三菱電機によるオープンなデザインシステムです。",
 };
