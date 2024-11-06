c1b0c7d1b30c28f03a2564fc2d37f7c1300fb0b2
degoo <degoo@takram.com>
2024-11-07 05:18:12 +0900

    Replace .png to .avif

--- src/components/toppage/ShowcaseCarouselWrapper.tsx
+++ src/components/toppage/ShowcaseCarouselWrapper.tsx
@@ -58,17 +58,17 @@ export const ShowcaseCarouselWrapper: React.FC = () => {
     <ShowcaseCarousel>
       <ShowcaseImageWrapper key={1}>
         <ShowcaseImageContainer>
-          <img src="/assets/pages/top/case1.png" alt="showcase" />
+          <img src="/assets/pages/top/case1.avif" alt="showcase" />
         </ShowcaseImageContainer>
       </ShowcaseImageWrapper>
       <ShowcaseImageWrapper key={2}>
         <ShowcaseImageContainer>
-          <img src="/assets/pages/top/case2.png" alt="showcase" />
+          <img src="/assets/pages/top/case2.avif" alt="showcase" />
         </ShowcaseImageContainer>
       </ShowcaseImageWrapper>
       <ShowcaseImageWrapper key={3}>
         <ShowcaseImageContainer>
-          <img src="/assets/pages/top/case3.png" alt="showcase" />
+          <img src="/assets/pages/top/case3.avif" alt="showcase" />
         </ShowcaseImageContainer>
       </ShowcaseImageWrapper>
     </ShowcaseCarousel>
