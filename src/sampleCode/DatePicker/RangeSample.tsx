6458178a14130f5a2cc0e2152b820823f287d81b
nagayama <nagayama@iroirodesign.com>
2025-09-03 16:41:06 +0900

    DatePickerのページを追加

---
+++ src/sampleCode/DatePicker/RangeSample.tsx
@@ -0,0 +1,5 @@
+import { DatePicker } from "@serendie/ui";
+
+export function RangeSample() {
+  return <DatePicker label="期間を選択" selectionMode="range" />;
+}
---
+++ src/sampleCode/DatePicker/StateSample.tsx
@@ -0,0 +1,38 @@
+import { parseDate } from "@ark-ui/react";
+import { DatePicker } from "@serendie/ui";
+import { Dd, Dl, Dt } from "src/components/LayoutUtils";
+
+export function StateSample() {
+  return (
+    <Dl>
+      <Dt>Enabled</Dt>
+      <Dd>
+        <DatePicker label="日付" placeholder="YYYY/MM/DD" />
+      </Dd>
+      <Dt>Filled</Dt>
+      <Dd>
+        <DatePicker
+          label="日付"
+          placeholder="YYYY/MM/DD"
+          required
+          value={[parseDate("2025-09-03")]}
+        />
+      </Dd>
+      <Dt>Error</Dt>
+      <Dd>
+        <DatePicker
+          label="日付"
+          placeholder="YYYY/MM/DD"
+          required
+          invalid
+          invalidMessage="正しい日付を入力してください"
+          value={[parseDate("2025-09-03")]}
+        />
+      </Dd>
+      <Dt>Disabled</Dt>
+      <Dd>
+        <DatePicker label="日付" placeholder="YYYY/MM/DD" disabled />
+      </Dd>
+    </Dl>
+  );
+}
